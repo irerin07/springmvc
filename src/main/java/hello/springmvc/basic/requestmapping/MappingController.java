8f13773c02fb4591d77df2ea8472635a84fced45
irerin07 <counterchord89@gmail.com>
2021-05-15 12:47:06 +0900

    inflearn spring 1

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -2,10 +2,7 @@ package hello.springmvc.basic.requestmapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MappingController {
@@ -26,6 +23,43 @@ public class MappingController {
     @GetMapping("/mapping-get-v2")
     public String mappingGetV2(){
         log.info("mappingGetV2");
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String userId){
+        log.info("mapping Id: {}", userId);
+        return "ok";
+    }
+
+    @GetMapping("/mapping/users/{userId}/orders/{orders}")
+    public String mappingPaths(@PathVariable String userId, @PathVariable Long orders){
+        log.info("mapping Id: {}", userId);
+        log.info("mapping Id: {}", orders);
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-param", params = "mode-debug")
+    public String mappingParam(){
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-header", params = "mode-debug")
+    public String mappingHeader(){
+        log.info("mappingHeader");
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-consume", consumes = "application/json")
+    public String mappingConsume(){
+        log.info("mappingConsume");
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-produce", consumes = "text/html")
+    public String mappingProduce(){
+        log.info("mappingProduce");
         return "ok";
     }
 }
